d7815b13e3e228f1442f2e74e8f682badf5ad62c
leopempem <leopemribeiro@gmail.com>
2023-01-20 08:34:54 -0300

    Final do programa

--- course17-01/src/application/Program.java
+++ course17-01/src/application/Program.java
@@ -28,7 +28,24 @@ public class Program {
 		 * product.quantity);
 		 */
 		
-		System.out.println(product);
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Update data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Update data: " + product);
 		
 		sc.close();
 	}
